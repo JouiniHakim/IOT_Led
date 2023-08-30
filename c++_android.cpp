fd109c03c7f77ddbf8aa7597bdd682ce9200e095
JouiniHakim <143658572+JouiniHakim@users.noreply.github.com>
2023-08-30 23:16:31 +0200

    Add files via upload

---
+++ c++_android.cpp
@@ -0,0 +1,65 @@
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+ 
+
+
+const char* ssid = "TOPNET_5218";
+const char* password = "bz7jr189bp";
+const int ledPin = 18;
+
+IPAddress local_IP(192, 168, 1, 12); 
+IPAddress subnet(255, 255, 255, 0);
+IPAddress gateway(192, 168, 1, 1);
+IPAddress primaryDNS(192, 168, 1, 1); 
+IPAddress secondaryDNS(0, 0, 0, 0); 
+
+WiFiServer server(80);
+ 
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+
+ if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS))
+    {
+      Serial.println("Configuration Failed!");
+    }
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to Wifi \n");
+  Serial.println(WiFi.localIP());
+  
+  pinMode(ledPin, OUTPUT);
+  server.begin();
+}
+ 
+void loop() {
+  WiFiClient client = server.available();
+  if (client) {
+    Serial.println("Client connected.");
+    
+    while (client.connected()) {
+      if (client.available()) {
+        String request = client.readStringUntil('\r');
+        request.trim();
+        Serial.println("Received: " + request); 
+        if(request == "ON"){
+          digitalWrite(ledPin, HIGH);
+        }else{
+          digitalWrite(ledPin, LOW);
+
+        }
+        
+ 
+       
+       
+      }
+      
+
+    }
+    Serial.println("Client disconnected.");
+  }
+}
---
+++ socket_android.java
@@ -0,0 +1,102 @@
+package com.example.socket_webserver;
+
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+//import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+package com.example.socket_webserver;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+//import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.socket_webserver.databinding.ActivityMainBinding;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+
+
+
+        binding.on.setOnClickListener(v -> sendCommand("ON"));
+
+        binding.off.setOnClickListener(v -> sendCommand("OFF"));
+    }
+
+        private void sendCommand(final String command) {
+            ExecutorService service = Executors.newSingleThreadExecutor();
+            service.execute(new Runnable() {
+                @Override
+                public void run() {
+                    String serverAddress = "192.168.1.12";
+                    int serverPort = 80;
+                    try {
+                        Socket socket = new Socket(serverAddress, serverPort);
+                        PrintWriter output = null;
+                        output = new PrintWriter(socket.getOutputStream(), true);
+                        output.println(command);
+                        socket.close();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+
+
+                }
+            });
+
+
+
+        }}
+
+////////////////////xml
+
+
+
+
+
+ <Button
+        android:id="@+id/on"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:layout_centerVertical="true"
+        android:layout_centerHorizontal="true"
+        android:text="ON" />
+
+    <Button
+        android:id="@+id/off"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:layout_centerHorizontal="true"
+        android:layout_below="@+id/on"
+        android:text="OFF" />
